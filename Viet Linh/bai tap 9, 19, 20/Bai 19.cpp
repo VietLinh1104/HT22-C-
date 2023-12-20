e42a059174d7c2800a19300a46819a211a68d41d
vietlinh <linhtx2004@gmail.com>
2023-12-21 05:38:57 +0700

    h

--- Viet Linh/bai tap 9, 19, 20/Bai 19.cpp
+++ Viet Linh/bai tap 9, 19, 20/Bai 19.cpp
@@ -25,35 +25,35 @@ class PhanSo1{
 		PhanSo1();
 		PhanSo1(int ts, int ms);
 
-		//29-30 tạo phương thức nhập xuất nạp chồng toán tử 
+		//29-30 tao phuong thuc nhap xuat nap chong toan tu 
 		friend istream& operator >> (istream& in, PhanSo1& obj);
 		friend ostream& operator << (ostream& out, PhanSo1 obj);
 
-		//33 tạo phương thức tối giản của class PhanSo1
+		//33 tao phuong thuc toi gian cua phanso1
 		void toi_gian();
 };
 
-//xây dựng các phương thức đã tạo của class PhanSo1
-// 38-41 hàm tạo
+//xay dung cac phuong thuc da tao cua class
+// 38-41 ham tao
 PhanSo1::PhanSo1(){
 	ts = 0;
 	ms = 0;
 }
 
-// hàm tạo
+// 44-47 ham tao
 PhanSo1::PhanSo1(int ts, int ms){
 	this -> ts = ts;
 	this -> ms = ms;
 }
 
-// xây dựng phương thức tối giản phân số
+// 50-54 xay dung phuong thuc toi gian phan so
 void PhanSo1::toi_gian(){
 	int uc = UCLN(ts,ms);
 	ts = ts/uc;
 	ms = ms/uc;
 }
 
-// phương thức nhập bằng operator
+// 57-67 phuong thuc nhap bang operator
 istream& operator >> (istream& in, PhanSo1&obj){
 	cout<<"Nhap tu so: "; in >> obj.ts;
 	
@@ -66,16 +66,16 @@ istream& operator >> (istream& in, PhanSo1&obj){
 	}while(obj.ms <= 0);
 }
 
-// phương thức xuất bằng operator
+// 70-73 phuong thuc xuat bang operator
 ostream& operator << (ostream& out, PhanSo1 obj){
 	out<<obj.ts<<"/"<<obj.ms;
 	return out;
 }
 
-// class phân số 2 kế thừa của class phân số 1
+// 76-93 class phanso2 ke thua cua class phanso1
 class PhanSo2:public PhanSo1{
 	public:
-		//tạo hàm tạo
+		//79-80 tao ham tao
 		PhanSo2();
 		PhanSo2(int ts, int ms);
 
@@ -92,35 +92,46 @@ class PhanSo2:public PhanSo1{
 		}
 };
 
-//xây dựng các phương thức đã tạo của class PhanSo2
-//hàm tạo
+//xay dung phuong thuc da tao trong class
+// 97-100 ham tao
 PhanSo2::PhanSo2(){
 	ts = 0;
 	ms = 0;
 }
 
-//hàm tạo
+// 103-106 ham tao
 PhanSo2::PhanSo2(int ts, int ms):PhanSo1(ts,ms){
 	this -> ts = ts;
 	this -> ms = ms;
 }
 
+// ham main
 int main(){
+	// 111-112 nhap n
 	int n;
 	cout << "Nhap N: "; cin >> n;
+
+	//115 khai bao DOI TUONG thuoc class phanso2
 	PhanSo2 a[n];
 
+	//118-122 dung ham for de nhap n DOI TUONG a
 	for(int i=0; i<n; i++){
 		cout << "Nhap phan so "<<i << ": \n";
 		cin >> a[i];
 		cout << endl;
 	}
+
+	//125 doi tuong "ps_max" va gan gia tri cua doi tuong "a[0]"
 	PhanSo2 ps_max = a[0];
+
+	//128-132 dung for de tim ps nho nhat
 	for(int i=1; i<n; i++){
 		if(a[i] > ps_max){
 			ps_max = a[i];
 		}
 	}
+
 	cout <<"Phan so lon nhat: "<< ps_max;
-	return 0;
+	cout << endl;
+	system("pause");
 }
