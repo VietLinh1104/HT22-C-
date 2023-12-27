f3e3423c9cb0030c6f2befa14feef3c1cb672879
vietlinh <linhtx2004@gmail.com>
2023-12-27 18:25:40 +0700

    haha

--- Viet Linh/leetcode/bai 1.cpp
+++ Viet Linh/leetcode/bai 1.cpp
@@ -3,47 +3,40 @@
 #include<iomanip>
 #include<math.h>
 #include<vector>
-#include<unordered_map>
+//#include<unordered_map>
 
 using namespace std;
 
 class Solution {
     public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numMap;
         int n = nums.size();
-
-        // Build the hash table
-        for (int i = 0; i < n; i++) {
-            numMap[nums[i]] = i;
-        }
-
-        // Find the complement
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.count(complement) && numMap[complement] != i) {
-                return {i, numMap[complement]};
+        for(int i=0; i<n; i++){
+//            cout <<"(i = "<<i<<")";
+            for(int j=i+1; j<n; j++){
+//                cout << "(j = "<<j<<")";
+                if( nums[i]+nums[j] == target){
+                    cout <<"{"<< i <<","<<j<<"}";
+					return {i,j} ;
+                }
             }
         }
-
         return {}; // No solution found
     }
 };
 
 int main(){
     vector<int> nums;
-    nums.push_back(7);
-    nums.push_back(6);
     nums.push_back(2);
+    nums.push_back(7);
+    nums.push_back(11);
+    nums.push_back(15);
     Solution a;
 
-    vector<int> b = a.twoSum(nums,9);
-    cout <<"{"<<b[0]<<","<< b[1]<<"}"<<endl;
+   vector<int> b = a.twoSum(nums,6);
+   cout <<b[0];
+    // a.twoSum(nums,9);
 
-	int n = nums.size();
-    for(int i=0; i<n; i++){
-        cout << nums[i];
-    }
 
     system("pause");
 }
